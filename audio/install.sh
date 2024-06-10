96ef396972a77e259b5a9cc1643bfa415b9b0853
Serialbocks <john@serialbocks.com>
2024-06-10 13:27:37 -0400

    Update install script

--- audio/install.sh
+++ audio/install.sh
@@ -1,5 +1,17 @@
-sudo apt-get install chromium-driver
+sudo apt-get install -y chromium-chromedriver
+
+git clone --recurse-submodules https://github.com/SeleniumHQ/selenium.git
+cd selenium/rust
+curl https://sh.rustup.rs -sSf > install-rust.sh
+chmod +x ./install-rust.sh
+./install-rust.sh -y
+. "$HOME/.cargo/env"
+cargo build
+
+sudo cp target/debug/selenium-manager /usr/bin/selenium-manager
 export SE_MANAGER_PATH=/usr/bin/selenium-manager
 echo "export SE_MANAGER_PATH=/usr/bin/selenium-manager" >> $HOME/.bashrc
 
-
+echo "Let's cleanup!"
+cd ../..
+rm -rf "selenium"
