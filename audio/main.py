5647fa7bde2bca27f1baee2ec42dfda247930080
Serialbocks <john@serialbocks.com>
2024-06-10 14:41:41 -0400

    Unmute automatically

--- audio/main.py
+++ audio/main.py
@@ -5,11 +5,30 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 def main():
+    
     driver = webdriver.Chrome()
     driver.get("http://192.168.0.180:8889/webcam")
     driver.maximize_window()
     body = driver.find_element(By.CSS_SELECTOR, "body")
+
     body.send_keys(Keys.CONTROL + Keys.HOME)
+    time.sleep(10)
+
+    video = driver.find_element(By.CSS_SELECTOR, "#video")
+    location = video.location
+    size = video.size
+    w, h = size['width'], size['height']
+    print(location)
+    print(size)
+    print(w, h)
+
+    xoffset = (w / 2) - 120
+    yoffset = (h / 2) - 23
+    print(xoffset, yoffset)
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(video, xoffset, yoffset)
+    action.click()
+    action.perform()
 
     while True:
         time.sleep(30)
