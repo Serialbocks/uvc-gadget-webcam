828b781819e6d90d832ae71cab79da7fee552351
Serialbocks <john@serialbocks.com>
2024-06-10 13:47:14 -0400

    wip

--- audio/main.py
+++ audio/main.py
@@ -2,15 +2,17 @@ import sys, os, time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 def main():
     driver = webdriver.Chrome()
-    driver.get("https://www.chess.com/analysis")
+    driver.get("http://192.168.0.180:8889/webcam")
     driver.maximize_window()
     body = driver.find_element(By.CSS_SELECTOR, "body")
     body.send_keys(Keys.CONTROL + Keys.HOME)
-    
-    time.sleep(15)
+
+    while True:
+        time.sleep(30)
 
 if __name__ == '__main__':
     try:
