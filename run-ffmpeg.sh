a1967cb34eab5c02c730d4ebd4f6d3db9988d6f3
Serialbocks <john@serialbocks.com>
2024-06-12 08:54:46 -0400

    Fix run-ffmpeg.sh

--- run-ffmpeg.sh
+++ run-ffmpeg.sh
@@ -1,3 +1,1 @@
-ffmpeg -f rtsp -i rtsp://127.0.0.1:8554/webcam -pix_fmt yuyv422 -f v4l2 /dev/video1
-#sleep(8)
-#/opt/uvc-gadget-webcam/uvc-gadget -v /dev/video1 -u /dev/video0 -n3 -f0 -r0 -s1 -o1
+ffmpeg -f rtsp -i rtsp://192.168.0.180:8554/webcam -pix_fmt yuyv422 -f v4l2 /dev/video1
